2c881fa7f122264d0f9e00678133eeaf053f7e5d
janahmedprg <jan.ahmed.prg@gmail.com>
2026-04-05 11:37:46 -0400

    fixed imports

--- src/modules/transformer_block.py
+++ src/modules/transformer_block.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
-from src.rope import RotaryPositionalEmbedding
-from src.multihead_self_attention import MultiHeadSelfAttention
-from src.rmsnorm import RMSNorm
-from src.swiglu import SwiGLU
+from src.modules.rope import RotaryPositionalEmbedding
+from src.modules.multihead_self_attention import MultiHeadSelfAttention
+from src.modules.rmsnorm import RMSNorm
+from src.modules.swiglu import SwiGLU
 
 class TransformerBlock(nn.Module):
 
